9bb29134564727cb30e41b588f690b0811138956
Haralan Dobrev <hdobrev@despark.com>
2012-06-19 21:07:38 +0300

    added trixxy.js and index.html

---
+++ trixxy.js
@@ -0,0 +1,33 @@
+(function( window, undefined ) {
+
+	function Stack( cards ) {
+		this.cards = cards;
+	}
+
+	Stack.isOK = function 
+
+	function Card( map, rotation, flip ) {
+
+		function processMap ( map ) {
+			return map;
+		}
+
+
+		this.map = processMap( map );
+		this.rotation = rotation;
+		this.flip = flip;
+		this.rows = Card.ROWS;
+		this.COLUMNS = Card.COLUMNS;
+	}
+
+	function Face()
+
+	/**
+	 * Card constants
+	 */
+
+	Card.COLORS = ['yellow', 'red', 'green', 'purple'];
+	Card.ROWS = 4;
+	Card.COLUMNS = 4;
+
+}( window ));
